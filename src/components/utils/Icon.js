9dc36c39489d04e7406a7e1da40e5f6b458970ee
pouyanjazini <p.jazini@gmail.com>
2024-03-01 23:50:15 +0330

    delete unused comments

--- src/components/utils/Icon.js
+++ src/components/utils/Icon.js
@@ -1,6 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
-// import { SearchOutlined } from "@ant-design/icons";
 
 const Icon = ({
   id,
@@ -15,7 +13,6 @@ const Icon = ({
   console.log("icon", icon);
   return (
     <>
-      {/* <SearchOutlined /> */}
       <i
         id={id}
         className={`icon ${fas ? "fas" : "far"} fa-${icon} ${
@@ -23,7 +20,7 @@ const Icon = ({
         }`}
         disabled={disabled}
         onClick={onClick}
-        style={{ fontSize: `${fontSize}px`, color: color }}
+        style={{ fontSize: `${fontSize}px`, color }}
       />
     </>
   );
@@ -32,17 +29,6 @@ const Icon = ({
 Icon.defaultProps = {
   fontSize: "4x",
   fas: false,
-};
-
-Icon.propTypes = {
-  id: PropTypes.string,
-  className: PropTypes.string,
-  icon: PropTypes.string,
-  color: PropTypes.string,
-  fas: PropTypes.bool,
-  fontSize: PropTypes.oneOf(["1x", "2x", "3x", "4x", "5x", "6x"]),
-  disabled: PropTypes.bool,
-  onClick: PropTypes.func,
 };
 
 export default Icon;
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,34 +1,12 @@
-import { combineReducers } from 'redux'
-import { people, person } from './person'
-import { post, posts } from './post'
+import { combineReducers } from "redux";
+import { people, person } from "./person";
+import { post, posts } from "./post";
 
 const reducer = combineReducers({
   people,
   person,
   posts,
-  post
-})
+  post,
+});
 
-// function reducer (
-//   state = { people: [], person: {}, posts: [], post: {} },
-//   { type, payload }
-// ) {
-//   switch (type) {
-//     case 'PEOPLE':
-//       return { ...state, people: payload }
-
-//     case 'PERSON':
-//       return { ...state, person: payload }
-
-//     case 'POSTS':
-//       return { ...state, posts: payload }
-
-//     case 'POST':
-//       return { ...state, post: payload }
-
-//     default:
-//       return state
-//   }
-// }
-
-export default reducer
+export default reducer;
